445b886c743bce07abfc53f98d54be02177fa336
haja <h.jagenteufel@gmail.com>
2019-07-10 10:59:38 +0200

    add localhost and trigger.lan as backends

--- zmessaging/src/main/scala/com/waz/service/BackendConfig.scala
+++ zmessaging/src/main/scala/com/waz/service/BackendConfig.scala
@@ -20,7 +20,7 @@ package com.waz.service
 import com.waz.service.BackendConfig.FirebaseOptions
 import com.waz.utils.wrappers.URI
 
-case class BackendConfig(baseUrl: URI, websocketUrl: String, firebaseOptions: FirebaseOptions, environment: String) {
+case class BackendConfig(baseUrl: URI, websocketUrl: String, firebaseOptions: FirebaseOptions, environment: String, name: String) {
   val pushSenderId = firebaseOptions.pushSenderId
 }
 
@@ -32,11 +32,13 @@ object BackendConfig {
   val StagingFirebaseOptions = FirebaseOptions("723990470614", "1:723990470614:android:9a1527f79aa62284", "AIzaSyAGCoJGUtDBLJJiQPLxHQRrdkbyI0wlbo8")
   val ProdFirebaseOptions    = FirebaseOptions("782078216207", "1:782078216207:android:d3db2443512d2055", "AIzaSyBdYVv2f-Y7JJmHVmDNCKgWvX6Isa8rAGA")
 
-  val StagingBackend = BackendConfig(URI.parse("http://10.0.2.2:8080"), "http://10.0.2.2:8080/await", StagingFirebaseOptions, "staging")
-  val ProdBackend    = BackendConfig(URI.parse("http://10.0.2.2:8080"), "http://10.0.2.2:8080/await", ProdFirebaseOptions,    "prod")
+  val StagingBackend = BackendConfig(URI.parse("http://10.0.2.2:8080"), "http://10.0.2.2:8080/await", StagingFirebaseOptions, "staging", "staging")
+  val StagingBackendLocal = BackendConfig(URI.parse("http://localhost:8080"), "http://localhost:8080/await", StagingFirebaseOptions, "staging", "local")
+  val StagingBackendTrigger = BackendConfig(URI.parse("http://trigger.lan:8080"), "http://trigger.lan:8080/await", StagingFirebaseOptions, "staging", "trigger")
+  val ProdBackend    = BackendConfig(URI.parse("http://10.0.2.2:8080"), "http://10.0.2.2:8080/await", ProdFirebaseOptions,    "prod", "prod")
 
-  lazy val byName = Seq(StagingBackend, ProdBackend).map(b => b.environment -> b).toMap
+  lazy val byName = Seq(StagingBackend, StagingBackendLocal, StagingBackendTrigger, ProdBackend).map(b => b.name -> b).toMap
 
-  def apply(baseUrl: String): BackendConfig = BackendConfig(URI.parse(baseUrl), "", StagingFirebaseOptions, "") // XXX only use for testing!
+  def apply(baseUrl: String): BackendConfig = BackendConfig(URI.parse(baseUrl), "", StagingFirebaseOptions, "", "") // XXX only use for testing!
 
 }
